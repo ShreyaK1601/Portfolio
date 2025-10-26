e0f0a7506f3c907ac16167525e2d968aa861aec3
ShreyaK1601 <kaleshreya162001@gmail.com>
2025-10-25 18:40:41 -0700

    Fix typo in footer copyright text

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
                 </a>
             </div>
 
-            <span className="footer__copy">&#169; Shreya Kale All rigths reserved</span>
+            <span className="footer__copy">&#169; Shreya Kale All rights reserved</span>
         </div>
     </footer>
   )
